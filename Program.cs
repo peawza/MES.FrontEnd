436c745e605c84b488a05fb5a3a704920056328a
Supachai <supachai@CSIGROUPS.COM>
2025-02-01 18:18:47 +0700

    Update Core Utils

--- Program.cs
+++ Program.cs
@@ -1,20 +1,49 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using WEB.APP.Extensions;
 using WEB.APP.Localization;
 using WEB.APP.MvcWebApp.Navigation;
 
 var builder = WebApplication.CreateBuilder(args);
+IConfiguration configuration = builder.Configuration; // allows both to access and to set up the config
+IWebHostEnvironment environment = builder.Environment;
+ConfigureWebHostBuilder WebHost = builder.WebHost;
+
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllersWithViews(options =>
+{
+    options.ModelBinderProviders.Insert(0, new DateTimeModelBinderProvider());
+})
+.AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver())
+.AddRazorRuntimeCompilation();
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddMemoryCache();
+
+
+
 builder.Services.AddScoped<ISiteMapProvider, DbSiteMapProvider>();
 
 
 
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<DbMessageLocalizer>();
-//builder.Services.AddSingleton<IMessageLocalizer>();
-//builder.Services.AddSingleton<IMessageResources>();
-//builder.Services.AddSingleton<IMessageResources, DbMessageLocalizer>();
 builder.Services.AddSingleton<IMessageLocalizer, DbMessageLocalizer>();
+
+
+
+
+
+
+
+
+
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +54,22 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+#region Set up Time Zone
+var supportedCultures = new[] { "en" };
+
+var cultureOptions = ((IApplicationBuilder)app).ApplicationServices.GetRequiredService<IOptions<ApplicationCultureOptions>>();
+
+
+var localizationOptions = new RequestLocalizationOptions()
+{
+    SupportedUICultures = cultureOptions.Value.GetCultures(supportedCultures),
+    SupportedCultures = cultureOptions.Value.GetCultures("en")
+}.SetDefaultCulture("en");
+app.UseRequestLocalization(localizationOptions);
+#endregion
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -32,15 +77,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-//app.UseEndpoints(endpoints =>
-//{
-//    endpoints.MapControllerRoute(
-//       name: "MyArea",
-//       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-//    endpoints.MapControllerRoute(
-//        name: "default",
-//        pattern: "{controller=Home}/{action=Index}/{id?}");
-//});
+
 
 using (var scope = app.Services.CreateScope())
 {
